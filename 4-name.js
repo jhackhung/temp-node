81e898fe874bc8e34888639e4a497875e7451af9
JackHung <hungjack1222@gmial.com>
2022-04-04 18:42:54 +0800

    first commit

---
+++ 4-name.js
@@ -0,0 +1,8 @@
+// local
+const secret = 'SUPER SECRET'
+// share
+const john = 'john'
+const peter = 'peter'
+
+// one of the syntax：Object
+module.exports = { john, peter }
---
+++ 6-alternative-flavor.js
@@ -0,0 +1,8 @@
+// Another Syntax
+module.exports.items = ['item1', 'item2']
+
+const person = {
+    name: 'bob',
+}
+
+module.exports.singlePerson = person
