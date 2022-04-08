9be738258948f759880da1d0bb079b05194afc0c
JackHung <hungjack1222@gmial.com>
2022-04-08 18:30:00 +0800

    Updating

--- app.js
+++
@@ -1,7 +0,0 @@
-// Starting Operating System Process
-console.log('first');
-setTimeout(() => {
-    console.log('second')
-}, 0)
-console.log('third')
-// completed and excited operating system process
