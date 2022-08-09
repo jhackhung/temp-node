e884dd61a00599c9945a897d46a675e51b273290
JackHung <hungjack1222@gmial.com>
2022-08-09 10:03:22 +0800

    add additions

--- Express-tutorial/app.js
+++ Express-tutorial/app.js
@@ -1,3 +1,4 @@
+// Route params & Query String
 const express = require("express")
 const app = express()
 const { products } = require("./data")
@@ -21,7 +22,7 @@ app.get('/api/products/:productID', (req, res) => {
     if (!singleProduct) {
         return res.status(404).send('Product Does Not Exist')
     }
-    return res.json(singleProduct)
+    return res.json(singleProduct)  //Important to add return avoiding error
 })
 
 app.get('/api/products/:productID/reviews/:reviewID', (req,res) => {
@@ -51,5 +52,3 @@ app.get('/api/v1/query', (req, res) => {
 app.listen(5000, ()=> {
     console.log("Server is listening on Port 5000...");
 })
-
-// 6:11:00
